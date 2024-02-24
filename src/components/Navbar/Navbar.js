4f86c6e97a69609005449b2a85e6b8c8af8ae727
nikink334 <nikink334@gmail.com>
2024-02-24 08:53:38 +0530

    home page addition

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,19 +2,23 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         
-            <div className="navbar">
-                <span className="brand">My Website</span>
-                <Link to="/">Home</Link>
-                <Link to="/About">About</Link>
-                <Link to="/Destinastion">Destinastion</Link>
-                <Link to="/Offers">Offers</Link>
-                <Link to="/Seats">Seats</Link>
-                <Link to="/SignUp">SignUp</Link>
-                <Link to="/Login">Login</Link>
+        <nav className="navbar">
+            <div className="leftSide"> Airborne Adventures. </div>
+            <div className="rightSide">
+                <ul className="navUL">
+                    <li><Link to="/" className="menuItems">Home</Link></li>
+                    <li><Link to="/Seats" className="menuItems">Seats</Link></li>
+                    <li><Link to="/Destination" className="menuItems">Destination</Link></li>
+                    <li><Link to="/Offers" className="menuItems">Offers</Link></li>
+                    <li><Link to="/About" className="menuItems">About</Link></li>
+                    <li><Link to="/SignUp" className="menuItems">SignUp</Link></li>
+                    <li><Link to="/Login" className="menuItems">Login</Link></li>
+                </ul>
             </div>
-        
+        </nav>
+
     )
 }

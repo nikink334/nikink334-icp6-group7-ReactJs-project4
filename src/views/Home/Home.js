144da1e50e6ef6a40835e97bc24ff8ab241745cc
nikink334 <nikink334@gmail.com>
2024-02-24 09:16:57 +0530

    home page changes

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,13 +10,13 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    
+
     <div>
       <Navbar />
-      
+
       <div className="home-banner-container">
         <div className="home-text-section">
-        <h1 className="primary-heading">
+          <h1 className="primary-heading">
             Explore the Skies with Ease
           </h1>
           <p className="primary-text">
@@ -29,32 +29,36 @@ function Home() {
         <div className="home-bannerImage-container">
           <img src={aeroImg} alt="" />
         </div>
-      </div>  <br/><br/><br/>
+      </div> 
+      
+
+
+
       <div className="home-banner-container">
-  <div className="home-image-container">
-    <img src={secImg} alt="" />
-  </div>
-  <div className="home-info-section">
-    <h3 className='secHead'> About &nbsp; &nbsp;  </h3>
-    <h1 className="info-heading">
-      Explore the Skies with Ease
-    </h1>
-    <p className="info-text">
-      Our flight booking system offers a seamless experience, allowing you to effortlessly plan your travels and soar to your desired destinations. With user-friendly interfaces and a wide range of options, booking flights has never been easier. Whether you're jetting off for business or leisure, trust us to provide you with the best deals and smoothest journey possible.
-    </p>
+        <div className="home-image-container">
+          <img src={secImg} alt="" />
+        </div>
+        <div className="home-info-section">
+          <h3 className='secHead'> About &nbsp; &nbsp;  </h3>
+          <h1 className="info-heading">
+            Explore the Skies with Ease
+          </h1>
+          <p className="info-text">
+            Our flight booking system offers a seamless experience, allowing you to effortlessly plan your travels and soar to your desired destinations. With user-friendly interfaces and a wide range of options, booking flights has never been easier. Whether you're jetting off for business or leisure, trust us to provide you with the best deals and smoothest journey possible.
+          </p>
 
 
-    <div className="button-container">
-  <Link to="/about" className="info-button">
-    Learn More
-  </Link>
-  <Link to="/about" className="info-button">
-    Watch Video
-  </Link>
-</div>
+          <div className="button-container">
+            <Link to="/about" className="info-button">
+              Learn More
+            </Link>
+            <Link to="/about" className="info-button">
+              Watch Video
+            </Link>
+          </div>
 
-  </div>
-</div>
+        </div>
+      </div>
 
 
       <Footer />
